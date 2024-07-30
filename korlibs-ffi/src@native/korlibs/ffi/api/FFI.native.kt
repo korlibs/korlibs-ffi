301d342d4fc1e7d8eb3773300c39bc9593699b00
soywiz <soywiz@gmail.com>
2024-07-30 13:49:58 +0200

    Some fixes

--- korlibs-ffi/src@native/korlibs/ffi/api/FFI.native.kt
+++ korlibs-ffi/src@native/korlibs/ffi/api/FFI.native.kt
@@ -6,9 +6,6 @@ import kotlinx.cinterop.*
 
 actual val isSupportedFFI: Boolean = true
 
-@OptIn(ExperimentalForeignApi::class)
 expect fun FFIDLOpen(name: String): COpaquePointer?
-@OptIn(ExperimentalForeignApi::class)
 expect fun FFIDLClose(lib: COpaquePointer?): Unit
-@OptIn(ExperimentalForeignApi::class)
 expect fun FFIDLSym(lib: COpaquePointer?, name: String): COpaquePointer?
--- korlibs-ffi/src@posix/korlibs/ffi/api/FFI.posix.kt
+++ korlibs-ffi/src@posix/korlibs/ffi/api/FFI.posix.kt
@@ -1,5 +1,10 @@
+@file:OptIn(ExperimentalForeignApi::class)
+
 package korlibs.ffi.api
 
-actual fun FFIDLOpen(name: String): COpaquePointer? = dlopen(name, platform.posix.RTLD_LAZY or 1)
-actual fun FFIDLClose(lib: COpaquePointer?): Unit { dlclose(lib) }
-actual fun FFIDLSym(lib: COpaquePointer?, name: String): COpaquePointer? = dlsym(lib, name)
+import kotlinx.cinterop.*
+import platform.posix.*
+
+actual fun FFIDLOpen(name: String): COpaquePointer? = platform.posix.dlopen(name, platform.posix.RTLD_LAZY or 1)
+actual fun FFIDLClose(lib: COpaquePointer?): Unit { platform.posix.dlclose(lib) }
+actual fun FFIDLSym(lib: COpaquePointer?, name: String): COpaquePointer? = platform.posix.dlsym(lib, name)
