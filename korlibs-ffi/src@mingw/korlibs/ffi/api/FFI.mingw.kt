bba554b1b6c40a37a6562edd9c7ee071a18ddd52
Marko Koschak <marko.koschak@tisno.de>
2026-04-20 14:51:09 +0200

    Decommonize transferMemory from native target

--- korlibs-ffi/src@mingw/korlibs/ffi/api/FFI.mingw.kt
+++ korlibs-ffi/src@mingw/korlibs/ffi/api/FFI.mingw.kt
@@ -3,8 +3,22 @@
 package korlibs.ffi.api
 
 import kotlinx.cinterop.*
+import platform.posix.memcpy
 import platform.windows.*
 
 actual fun FFIDLOpen(name: String): COpaquePointer? = LoadLibraryW(name)?.reinterpret()
 actual fun FFIDLClose(lib: COpaquePointer?): Unit { FreeLibrary(lib?.reinterpret()) }
 actual fun FFIDLSym(lib: COpaquePointer?, name: String): COpaquePointer? = GetProcAddress(lib?.reinterpret(), name)?.reinterpret()
+
+internal actual fun transferMemory(address: Long, data: ByteArray, offset: Int, size: Int, toPointer: Boolean) {
+    if (size == 0) return
+    data.usePinned {
+        val arrayPtr = it.addressOf(offset)
+        val pointerPtr = address.toCPointer<ByteVar>()
+        if (toPointer) {
+            memcpy(arrayPtr, pointerPtr, size.convert())
+        } else {
+            memcpy(pointerPtr, arrayPtr, size.convert())
+        }
+    }
+}
--- korlibs-ffi/src@posix/korlibs/ffi/api/FFI.posix.kt
+++ korlibs-ffi/src@posix/korlibs/ffi/api/FFI.posix.kt
@@ -8,3 +8,16 @@ import platform.posix.*
 actual fun FFIDLOpen(name: String): COpaquePointer? = platform.posix.dlopen(name, platform.posix.RTLD_LAZY or 1)
 actual fun FFIDLClose(lib: COpaquePointer?): Unit { platform.posix.dlclose(lib) }
 actual fun FFIDLSym(lib: COpaquePointer?, name: String): COpaquePointer? = platform.posix.dlsym(lib, name)
+
+internal actual fun transferMemory(address: Long, data: ByteArray, offset: Int, size: Int, toPointer: Boolean) {
+    if (size == 0) return
+    data.usePinned {
+        val arrayPtr = it.addressOf(offset)
+        val pointerPtr = address.toCPointer<ByteVar>()
+        if (toPointer) {
+            memcpy(arrayPtr, pointerPtr, size.convert())
+        } else {
+            memcpy(pointerPtr, arrayPtr, size.convert())
+        }
+    }
+}
